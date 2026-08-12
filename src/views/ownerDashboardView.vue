de6c8a6a64b37e385ed663fc01043ff2981d7147
r0963241 <r0963241@student.thomasmore.be>
2026-08-12 11:30:58 +0200

    fixed revenue

--- src/views/ownerDashboardView.vue
+++ src/views/ownerDashboardView.vue
@@ -365,7 +365,7 @@ export default {
       const amount = Number(value || 0);
       return new Intl.NumberFormat('en-US', {
         style: 'currency',
-        currency: 'USD',
+        currency: 'EUR',
         maximumFractionDigits: 2
       }).format(amount);
     },
